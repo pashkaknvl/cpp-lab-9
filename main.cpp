a44ae5719ed5a804245e20af780bffbfda6a885b
pashkaknvl <pashak.konovalov05@gmail.com>
2024-02-05 22:23:07 +0300

    1 commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+template<typename Type1, typename Type2>
+void conversion(Type1* currentArray, Type2* updatedArray, size_t size){
+    for (size_t i = 0; i < size; i++)
+        updatedArray[i] = static_cast<Type2>(currentArray[i]);
+}
+
+
+int main() {
+    int array[] = {1, 5, 7, 2, 10};
+    float array1[5];
+    char array2[5];
+
+    conversion<int,float>(array, array1, 5);
+    conversion<int,char>(array, array2, 5);
+
+    for (size_t i = 0; i < 5; i++)
+        cout << array1[i] << " ";
+    cout << endl;
+    for (size_t i = 0; i < 5; i++)
+        cout << array2[i] << " ";
+
+    cout << typeid(array1[0]).name();
+
+    return 0;
+}
